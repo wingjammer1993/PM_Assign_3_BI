ef78c426b994a69a0dcbc026edc28dcbd4a31239
Amruta <hail.radical@gmail.com>
2018-02-15 21:14:25 -0700

    assignment

--- Part_ii.py
+++ Part_ii.py
@@ -73,34 +73,21 @@ exampleC2 = [
     [0,0,0,0,0,0,0,1,0,0],
     [0,0,0,0,0,0,0,1,0,0],
     [0,0,0,0,0,0,0,1,0,0],
-];
+]
 
-# Plot 1
-exampleA2 = np.multiply(exampleA2, 2)
-example = np.add(np.matrix(exampleA1), np.matrix(exampleA2))
-cmap = colors.ListedColormap(['black', 'red', 'green'])
-fig, ax = plt.subplots()
-ax.set_xticks(np.arange(0, 10, 1))
-ax.set_yticks(np.arange(9, -1, -1))
-plt.imshow(example, cmap=cmap)
-plt.show()
 
-# Plot 2
-exampleB2 = np.multiply(exampleB2, 2)
-example = np.add(np.matrix(exampleB1), np.matrix(exampleB2))
-cmap = colors.ListedColormap(['black', 'red', 'green', 'yellow'])
-fig2, ax = plt.subplots()
-ax.set_xticks(np.arange(0, 10, 1))
-ax.set_yticks(np.arange(9, -1, -1))
-plt.imshow(example, cmap=cmap)
-plt.show()
+def plot_grid(ex1, ex2):
+    ex2 = np.multiply(ex2, 2)
+    example = np.add(np.matrix(ex1), np.matrix(ex2))
+    cmap = colors.ListedColormap(['black', 'red', 'green', 'yellow'])
+    fig, ax = plt.subplots()
+    ax.set_xticks(np.arange(0, 10, 1))
+    ax.set_yticks(np.arange(9, -1, -1))
+    plt.imshow(example, cmap=cmap)
+    plt.show()
 
-# Plot 3
-exampleC2 = np.multiply(exampleC2, 2)
-example = np.add(np.matrix(exampleC1), np.matrix(exampleC2))
-cmap = colors.ListedColormap(['black', 'red', 'green', 'yellow'])
-fig3, ax = plt.subplots()
-ax.set_xticks(np.arange(0, 10, 1))
-ax.set_yticks(np.arange(9, -1, -1))
-plt.imshow(example, cmap=cmap)
-plt.show()
+
+if __name__ == "__main__":
+    plot_grid(exampleA1, exampleA2)
+    plot_grid(exampleB1, exampleB2)
+    plot_grid(exampleC1, exampleC2)
