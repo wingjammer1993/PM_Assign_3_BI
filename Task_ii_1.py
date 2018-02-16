9d102e94f2ecc295383198d9072216af499e3a0a
Amruta <hail.radical@gmail.com>
2018-02-15 22:30:32 -0700

    assignment

---
+++ Task_ii_1.py
@@ -0,0 +1,126 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from matplotlib import colors
+import math
+
+
+def plot_velocity_maps(ex1, ex2, v):
+    probability_map = np.zeros((len(v), len(v)))
+    for idx1, vel1 in enumerate(v):
+        for idx2, vel2 in enumerate(v):
+            log_prob = 0
+            for x in range(0, 10):
+                for y in range(0, 10):
+                    log_likelihood = give_likelihood(ex1, ex2, x, y, vel1, vel2)
+                    log_prob = log_prob + log_likelihood
+            probability_map[idx1, idx2] = log_prob
+    return probability_map
+
+
+def give_likelihood(frame_1, frame_2, x_pos, y_pos, velocity_x, velocity_y):
+    int_1 = frame_1[x_pos, y_pos]
+    if 0 <= x_pos+velocity_x < 10 and 0 <= y_pos+velocity_y < 10:
+        int_2 = frame_2[x_pos+velocity_x, y_pos+velocity_y]
+    else:
+        int_2 = 0
+    exponent = ((int_1-int_2)**2)
+    likelihood = math.exp(-exponent)
+    likelihood_log = math.log(likelihood)
+    return likelihood_log
+
+
+def plot_map(map_prob):
+    fig, ax = plt.subplots()
+    # ax.set_xticks([-2, -1, 0, 1, 2])
+    # ax.set_yticks([-2, -1, 0, 1, 2])
+    plt.imshow(map_prob, cmap='gist_heat')
+    plt.colorbar()
+    plt.show()
+
+
+if __name__ == "__main__":
+    exampleA1 = [
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    ];
+    exampleA2 = [
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    ];
+    exampleB1 = [
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    ];
+    exampleB2 = [
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    ];
+    exampleC1 = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    ];
+    exampleC2 = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    ]
+
+    vel = [-2, -1, 0, 1, 2]
+    map_1 = plot_velocity_maps(np.array(exampleA1), np.array(exampleA2), vel)
+    plot_map(map_1)
+
+    map_2 = plot_velocity_maps(np.array(exampleB1), np.array(exampleB2), vel)
+    plot_map(map_2)
+
+    map_3 = plot_velocity_maps(np.array(exampleC1), np.array(exampleC2), vel)
+    plot_map(map_3)
+    print('done')
+
+
