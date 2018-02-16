800ce9b181a06cbaf5652aeb72a24d171a243728
Amruta <hail.radical@gmail.com>
2018-02-16 11:51:54 -0700

    assignment

---
+++ Task_ii_2.py
@@ -0,0 +1,115 @@
+import math
+import numpy as np
+import Task_ii_1
+
+
+def give_posterior_map(likelihood_map, sigma_p, velocity):
+    prior_map = np.zeros_like(likelihood_map)
+    for idx1, vx in enumerate(velocity):
+        for idx2, vy in enumerate(velocity):
+            prior_map[idx1, idx2] = give_prior(vx, vy, sigma_p)
+    Task_ii_1.plot_map(prior_map)
+    log_posterior = np.add(prior_map, likelihood_map)
+    return log_posterior
+
+
+def give_prior(vel_x,vel_y, sigma_p):
+    norm = (vel_x**2 + vel_y**2)**0.5
+    constant = 2*(sigma_p**2)
+    exponent = -norm/constant
+    prior = math.exp(-exponent)
+    prior_log = math.log(prior)
+    return prior_log
+
+
+if __name__ == "__main__":
+
+    exampleA1 = [
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    ];
+    exampleA2 = [
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    ];
+    exampleB1 = [
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    ];
+    exampleB2 = [
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    ];
+    exampleC1 = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    ];
+    exampleC2 = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    ]
+
+    vel = [-2, -1, 0, 1, 2]
+    sig_p = 1.414
+    map_likelihood = Task_ii_1.plot_velocity_maps(np.array(exampleA1), np.array(exampleA2), vel)
+    map_posterior = give_posterior_map(map_likelihood, sig_p, vel)
+    Task_ii_1.plot_map(map_posterior)
+
+    map_likelihood2 = Task_ii_1.plot_velocity_maps(np.array(exampleB1), np.array(exampleB2), vel)
+    map_posterior2 = give_posterior_map(map_likelihood2, sig_p, vel)
+    Task_ii_1.plot_map(map_posterior2)
+
+    map_likelihood3 = Task_ii_1.plot_velocity_maps(np.array(exampleC1), np.array(exampleC2), vel)
+    map_posterior3 = give_posterior_map(map_likelihood3, sig_p, vel)
+    Task_ii_1.plot_map(map_posterior3)
+
+    print('done')
+
