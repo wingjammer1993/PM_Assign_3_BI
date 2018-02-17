109e01836bc40487036d7ad4feb0fa4830f0b685
Amruta <hail.radical@gmail.com>
2018-02-17 08:56:03 -0700

    assignment

--- Task_ii_3.py
+++ Task_ii_3.py
@@ -1,30 +1,13 @@
-import math
-import numpy as np
 import Task_ii_1
+import Task_ii_2
+import numpy as np
+import math
 
 
-def give_posterior_map(likelihood_map, sigma_p, velocity):
-    prior_map = np.zeros_like(likelihood_map)
-    for idx1, vx in enumerate(velocity):
-        for idx2, vy in enumerate(velocity):
-            prior_map[idx1, idx2] = give_prior(vx, vy, sigma_p)
-    # normalize the prior
-    summation = np.sum(prior_map)
-    prior_map = np.divide(prior_map, summation)
-    # find log prior
-    prior_map = np.log(prior_map)
-    Task_ii_1.plot_map(prior_map)
-    # find log posterior
-    log_posterior = np.add(prior_map, likelihood_map)
-    return log_posterior
-
-
-def give_prior(vel_x, vel_y, sigma_p):
-    norm = vel_x**2 + vel_y**2
-    constant = 2*(sigma_p**2)
-    exponent = norm/constant
-    prior = math.exp(-exponent)
-    return prior
+def normalize_posterior(posterior_map):
+    scaling_factor = math.frexp(np.prod(posterior_map))[0]
+    scaled_posterior_map = np.float64(np.subtract(posterior_map, scaling_factor))
+    return scaled_posterior_map
 
 
 if __name__ == "__main__":
@@ -106,16 +89,18 @@ if __name__ == "__main__":
     # sigma_sqr = 0.5 so, sigma_p = 1
     sig_p = 1
     map_likelihood = Task_ii_1.plot_velocity_maps(np.array(exampleA1), np.array(exampleA2), vel)
-    map_posterior = give_posterior_map(map_likelihood, sig_p, vel)
-    Task_ii_1.plot_map(map_posterior)
+    map_posterior = Task_ii_2.give_posterior_map(map_likelihood, sig_p, vel)
+    scale_posterior = normalize_posterior(map_posterior)
+    Task_ii_1.plot_map(scale_posterior)
 
     map_likelihood2 = Task_ii_1.plot_velocity_maps(np.array(exampleB1), np.array(exampleB2), vel)
-    map_posterior2 = give_posterior_map(map_likelihood2, sig_p, vel)
-    Task_ii_1.plot_map(map_posterior2)
+    map_posterior2 = Task_ii_2.give_posterior_map(map_likelihood2, sig_p, vel)
+    scale_posterior2 = normalize_posterior(map_posterior2)
+    Task_ii_1.plot_map(scale_posterior2)
 
     map_likelihood3 = Task_ii_1.plot_velocity_maps(np.array(exampleC1), np.array(exampleC2), vel)
-    map_posterior3 = give_posterior_map(map_likelihood3, sig_p, vel)
-    Task_ii_1.plot_map(map_posterior3)
+    map_posterior3 = Task_ii_2.give_posterior_map(map_likelihood3, sig_p, vel)
+    scale_posterior3 = normalize_posterior(map_posterior3)
+    Task_ii_1.plot_map(scale_posterior3)
 
     print('done')
-
